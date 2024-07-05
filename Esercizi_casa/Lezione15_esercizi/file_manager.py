a824451188ff0aa3f61a5d66ad81e404b4c321d7
Monir Rafik <159887885+mraf01@users.noreply.github.com>
2024-07-05 12:33:35 +0200

    File Manager

---
+++ Esercizi_casa/Lezione15_esercizi/file_manager.py
@@ -0,0 +1,18 @@
+class FileManager:
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+    
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.file:
+            self.file.close()
+
+
+if __name__ == "__main__":
+    with FileManager('example.txt', 'w') as f:
+        f.write('Hello, world!')
